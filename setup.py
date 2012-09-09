aabd7fa2437907c5caf7636d156d59043b045a7a
slush <info@bitcoin.cz>
2012-09-09 06:27:36 +0200

    Improved setup.py, native py2exe support on Windows

--- setup.py
+++ setup.py
@@ -3,14 +3,35 @@ from distribute_setup import use_setuptools
 use_setuptools()
 
 from setuptools import setup
+import sys, os
+try:
+    import py2exe
+except ImportError:
+    py2exe = None
 
-setup(name='stratum_mining_proxy',
-      version='0.1.0',
-      description='Getwork-compatible proxy for Stratum mining pools',
-      author='slush',
-      author_email='info@bitcion.cz',
-      url='http://mining.bitcoin.cz/stratum-mining/',
-      py_modules=['midstate',],
-      install_requires=['twisted', 'stratum',],
-      scripts=['mining_proxy.py'],
-     )
+
+args = {
+    'name': 'stratum_mining_proxy',
+    'version': '0.1.1',
+    'description': 'Getwork-compatible proxy for Stratum mining pools',
+    'author': 'slush',
+    'author_email': 'info@bitcion.cz',
+    'url': 'http://mining.bitcoin.cz/stratum-mining/',
+    'py_modules': ['midstate',],
+    'install_requires': ['twisted', 'stratum',],
+    'scripts': ['mining_proxy.py'],
+}
+
+if py2exe != None:
+    args.update({
+        # py2exe options
+        'options': {'py2exe':
+                      {'optimize': 2,
+                       'bundle_files': 1,
+                       'compressed': True,
+                      },
+                  },
+        'console': ['mining_proxy.py'],
+    })
+
+setup(**args)
