33800dc0b5a14e5e79defe18044c1cf2c539151e
slush <info@bitcoin.cz>
2012-09-10 07:20:33 +0000

    version 0.3.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 args = {
     'name': 'stratum_mining_proxy',
-    'version': '0.3.0',
+    'version': '0.3.1',
     'description': 'Getwork-compatible proxy for Stratum mining pools',
     'author': 'slush',
     'author_email': 'info@bitcion.cz',
