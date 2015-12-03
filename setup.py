b776c30edb4127a6ab0f9b1ec5e0ede8f632896a
slush0 <slush@satoshilabs.com>
2015-12-03 15:47:21 +0100

    Removed dependency to midstatec module.

--- setup.py
+++ setup.py
@@ -14,25 +14,13 @@ from mining_libs import version
 args = {
     'name': 'stratum_mining_proxy',
     'version': version.VERSION,
-    'description': 'Getwork-compatible proxy for Stratum mining pools',
+    'description': 'Mining proxy for Stratum mining pools',
     'author': 'slush',
     'author_email': 'slush@satoshilabs.com',
     'url': 'http://mining.bitcoin.cz/stratum-mining/',
-    'ext_modules': [
-      Extension(
-        'midstate', 
-        ['midstatec/midstatemodule.c'],
-        include_dirs=['/usr/include/python2.7'],
-        extra_compile_args=['-march=native', '-Wall', '-funroll-all-loops', '-O3', '-fstrict-aliasing', '-Wall', '-std=c99',  '-fPIC', '-shared'],
-        libraries=['python2.7'],
-        extra_link_args=['-Wl,-O1', '-Wl,--as-needed']
-        )
-      ],
-    'py_modules': ['mining_libs.client_service', 'mining_libs.getwork_listener',
-                   'mining_libs.jobs', 'mining_libs.midstate',
+    'py_modules': ['mining_libs.client_service', 'mining_libs.jobs',
                    'mining_libs.multicast_responder', 'mining_libs.stratum_listener',
-                   'mining_libs.utils', 'mining_libs.version', 'mining_libs.worker_registry',
-                   'midstatec.midstatec'],
+                   'mining_libs.utils', 'mining_libs.version', 'mining_libs.worker_registry'],
     'install_requires': ['setuptools>=0.6c11', 'twisted>=12.2.0', 'stratum>=0.2.15', 'argparse'],
     'scripts': ['mining_proxy.py'],
 }
